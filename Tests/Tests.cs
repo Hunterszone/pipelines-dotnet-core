ca02bb029babf0d6062e01a697545f8b8da2112b
kdrenski <k.drenski91@gmail.com>
2024-02-24 17:52:12 +0200

    Adding unit tests

---
+++ Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pipelines_dotnet_core.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
